1c426abf70fdce092c695f2c2a8757f707f48095
WilliamDenega <williamdenega@gmail.com>
2022-04-12 11:30:25 -0400

    commit tree d9f492551baf6fc6eda5844da6da2ca566233076

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import Header from './Header';
+
+
 
 function App() {
+  
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [items,setItems] = useState(itemData)
 
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
@@ -11,13 +16,8 @@ function App() {
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={handleDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
-      <ShoppingList items={itemData} />
+      <Header isDarkMode={isDarkMode} onDarkModeClick ={handleDarkModeClick}/>
+      <ShoppingList items={items} />
     </div>
   );
 }
---
+++ src/components/Filter.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+
+
+function Header({onCategoryChange }){
+
+    return (
+        <select name="filter" onChange={onCategoryChange}>
+        <option value="All">Filter by category</option>
+        <option value="Produce">Produce</option>
+        <option value="Dairy">Dairy</option>
+        <option value="Dessert">Dessert</option>
+      </select>
+
+    )
+}
+
+
+export default Header
